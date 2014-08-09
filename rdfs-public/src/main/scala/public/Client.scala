8fe0e8b9399f59981dc2f50dcb4c1559787f95b5
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2014-08-08 19:20:06 -0700

    Elaborate Client code generation

--- rdfs-public/src/main/scala/public/Client.scala
+++ rdfs-public/src/main/scala/public/Client.scala
@@ -1,8 +1,34 @@
-package public
+package com.github.jedesah.thrift
 
-/**
- * Created by jdesjardins on 8/7/14.
- */
-class Client {
+import org.apache.thrift.TApplicationException
+import org.apache.thrift.protocol.{TMessageType, TMessage, TProtocol}
+import serialization._
 
+class Client(protocol: TProtocol) {
+
+  var seqId = 0
+
+  protected def send[T: ThriftCodec](methodName: String, args: T) {
+    seqId += 1
+    protocol.writeMessageBegin(new TMessage(methodName, TMessageType.CALL, seqId))
+    write(args, protocol)
+    protocol.writeMessageEnd()
+    protocol.getTransport.flush()
+  }
+
+  protected def receive[T: ThriftCodec](methodName: String): T = {
+    val msg = protocol.readMessageBegin()
+    if (msg.`type` == TMessageType.EXCEPTION) {
+      val exception = TApplicationException.read(protocol)
+      protocol.readMessageEnd()
+      throw exception
+    }
+    if (msg.seqid != seqId) {
+      val explanation = s"$methodName failed: out of sequence response"
+      throw new TApplicationException(TApplicationException.BAD_SEQUENCE_ID, explanation)
+    }
+    val result: T = read(protocol)
+    protocol.readMessageEnd()
+    result
+  }
 }
--- rdfs-public/src/main/scala/public/serialization.scala
+++ rdfs-public/src/main/scala/public/serialization.scala
@@ -10,5 +10,17 @@ package object serialization {
     def read(protocol: TProtocol): T
     def write(obj: T, protocol: TProtocol)
   }
+
+  object ThriftCodec {
+    implicit val intCodec = new ThriftCodec[Int] {
+      def read(protocol: TProtocol): Int = protocol.readI32()
+      def write(obj: Int, protocol: TProtocol) { protocol.writeI32(obj) }
+    }
+
+    implicit val shortCodec = new ThriftCodec[Short] {
+      def read(protocol: TProtocol): Short = protocol.readI16()
+      def write(obj: Short, protocol: TProtocol) { protocol.writeI16(obj) }
+    }
+  }
 }
 
